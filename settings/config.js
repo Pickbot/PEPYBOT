1672c12b50fa541008faa697a33dfa00ab6e95df
Pickbot <alphalgd@gmail.com>
2021-03-08 21:48:45 -0300

    First commit

---
+++ settings/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'PepyBot',
+        ownerName: 'Guilherme',
+        youtube: 'YOUTUBE_LINK',
+        instagram: 'https://instagram.com/hoshigaki_guilherme?igshid=10m4wafp30wdd',
+}
---
+++ src/menu.js
@@ -0,0 +1,12 @@
+const menu = (prefix, pushname) => {
+    return `◪ *Comandos do dono*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.menu = menu
